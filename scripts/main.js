faa689fc20af295e77dfd93a685de43e71f487d1
Safiyyh <safiyyah.jackmanwilliams@hotmail.co.uk>
2018-12-13 19:58:16 +0000

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,25 @@
-$(() => {
+$(window).on('load', function() {
 
-  new TypeIt('#hero-text-subtext', {
-    strings: ["photographing beautiful people and places"],
-    speed: 50,
-    breakLines: true,
-    autoStart: true,
-  });
+  // FANCY BOX
 
-//   $(window).('scroll', function() {
-//      if ($(window).scrollTop() > 5) {
-//          $('.hero-text-title').hide();
-//      }
-//      else {
-//          $('.hero-text-title').show();
-//      }
-// });
+  // const $gallery = $('[data-fancybox="gallery"]');
+  //
+  // if ($gallery.length) {
+  //   $gallery.fancybox({
+  //     protect: true
+  //   });
+  // }
 
-  $('#grid2 img').each(function(){
-      var $this = $(this);
-      $this.wrap('<div class="item"><a></a></div>');
-      $('a').removeAttr('href');
-  });
-  $('#grid2').addClass('effect-2');
 
-  $(window).load(function(){
-    var $container = $('.grid');
-    // initialize
-    $container.masonry({
-      //columnWidth: 200,
-      itemSelector: '.item'
-    })
-    $('.item > a').removeAttr('href')
+  const $container = $('.grid');
+  // initialize
+  $container.masonry({
+    //columnWidth: 200,
+    itemSelector: '.item'
   })
+  $('.item > a').removeAttr('href');
+
+
 
 
 });
