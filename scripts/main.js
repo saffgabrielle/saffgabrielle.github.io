ad4b23751594f398f5a3aea26887c57c343ac5d9
Safiyyh <safiyyah.jackmanwilliams@hotmail.co.uk>
2018-12-16 12:19:42 +0000

    back buttons and grid animations

--- scripts/main.js
+++ scripts/main.js
@@ -16,10 +16,17 @@ $(window).on('load', function() {
   $container.masonry({
     //columnWidth: 200,
     itemSelector: '.item'
-  })
+});
+
   $('.item > a').removeAttr('href');
 
-
+ScrollReveal().reveal('.item', { delay: 500 });
+ScrollReveal().reveal('.item', { easing: 'steps(10)' });
+ScrollReveal().reveal('.item', { interval: 100 });
+ScrollReveal().reveal('.item', {
+    distance: '0px',
+    opacity: 0.8
+});
 
 
 });
